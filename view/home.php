c47cf720990864ec26e651ffbcd308e74e972dd7
mimilgrp <mimil.group.49@gmail.com>
2024-04-05 09:38:39 +0300

    update home css

--- view/home.php
+++ view/home.php
@@ -1,3 +1,60 @@
-<div class="container container-fluid">
-    <h1>Welcome on Foodinni</h1>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h2>Welcome to your new favorite&nbsp;store</h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <h3>Discounts</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col d-none d-lg-block">
+            <img src="https://images.ctfassets.net/kt8yvydomzor/Gw9ksuMEQQh8vxmO9jXoR/2a6993954c84ce524722f39ac23f244f/Pirkkahintaan_2024_etusivun_paraati_2300x576.jpg" class="img-ad">
+            <p class="p-ref">Advertising</p>
+        </div>
+        <div class="col d-block d-lg-none">
+            <img src="https://images.ctfassets.net/kt8yvydomzor/40LnWOkIF1j1jKzTGoLMfe/5444224619c1e4222655486e262921db/Pirkka_brandilahto_omat_digimediat_2024_verkkosivukuva_1440x550.jpg?w=1440&h=550&fit=fill&f=top&q=90&fm=avif" class="img-ad">
+            <p class="p-ref">Advertising</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <h3>Categories</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <h3>Brands</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+            <h2>Use a Customer Account</h2>
+        </div>
+        <div class="col-lg-4">
+            <div class="div-tile">
+                <h4>Create a Customer Account on this website</h4>
+                <p>Select the Account icon at the top right to register on our platform and access customer benefits</p>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="div-tile">
+                <h4>Add your items to your new shopping list</h4>
+                <p>Select the items of your choice on our site then add them to your personal shopping list by clicking on the List icon</p>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="div-tile">
+                <h4>Find your items in one click for your next shopping</h4>
+                <p>Click the List icon again the next time you visit a Foodinni store, don't forget any items again</p>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <h2>Do you like Foodinni ?</h2>
+        </div>
+    </div>
 </div>
